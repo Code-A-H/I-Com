105ed8cbe79097a6c7a77d8b11c1b1d7d8d63db8
unknown <alex.sa471@yahoo.com>
2020-11-02 16:17:27 -0500

    s

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php
-$hola = "text";
+
+include("conexion.php");
+
+$nombre = $_POST["usuario"];
+$pass   = $_POST["pass"];
+
+//Login
+if(isset($_POST["btningresar"]))
+{
+	$query = mysqli_query($conn,"SELECT * FROM login WHERE usuario = '$nombre' AND password='$pass'");
+	$nr = mysqli_num_rows($query);
+	
+	if($nr==1)
+	{
+		echo "<script> alert('Bienvenido $nombre'); window.location='principal.html' </script>";
+	}else
+	{
+		echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
+	}
+}
+
+//Registrar
+if(isset($_POST["btnregistrar"]))
+{
+	$sqlgrabar = "INSERT INTO login(usuario,password) values ('$nombre','$pass')";
+	
+	if(mysqli_query($conn,$sqlgrabar))
+	{
+		echo "<script> alert('Usuario registrado con exito: $nombre'); window.location='index.html' </script>";
+	}else 
+	{
+		echo "Error: ".$sql."<br>".mysql_error($conn);
+	}
+}
+
 
 ?>
