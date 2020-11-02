b7cd53857d37bfad6fedf083964041f8f79432d0
harvis perez <harvim.201@hotmail.com>
2020-11-02 17:31:22 -0500

    c

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if(isset($_POST["btnIngresar"]))
 		echo "<script> alert('Bienvenido $nombre'); window.location='principal.html' </script>";
 	}else
 	{
-		echo "<script> alert('Usuario no existe'); </script>";
+		echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
     }///y
 
 }else{
@@ -31,7 +31,7 @@ if(isset($_POST["btnRegistrar"]))
 	
 	if(mysqli_query($conn,$sqlgrabar))
 	{
-		echo "<script> alert('Usuario registrado con exito: $nombre'); </script>";
+		echo "<script> alert('Usuario registrado con exito: $nombre'); window.location='index.html' </script>";
 	}else 
 	{
 		echo "Error: ".$sql."<br>".mysql_error($conn);
