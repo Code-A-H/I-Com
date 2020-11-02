597abb2cf32f27d50fd7853bedb485219a095ee9
unknown <alex.sa471@yahoo.com>
2020-11-02 16:18:32 -0500

    a

--- conexion.php
+++ conexion.php
@@ -1,1 +1,28 @@
-a
+<!--
+Crear un login y una página para registrar a un nuevo usuario con los siguientes datos:
+usuario y contraseña.
+
+
+(index.html) Login 	  -> Si el usuario existe 	 -> principal.html
+(index.html) Login    -> Si el usuario no existe -> (index.html) Login
+ registrar.html       -> Nuevo usuario           -> (index.html) Login
+
+El login tendrá la opción para registrar.
+Usar "estilos.css" en el ejercicio.
+
+-->
+
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "test2";
+
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+if(!$conn)
+{
+	die("No hay conexion:" .mysqli_connect_error());
+}
+?>
