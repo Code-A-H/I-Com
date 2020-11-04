792bd8e50b001770314ca7fd0fb08a6293cf6762
harvis perez <harvim.201@hotmail.com>
2020-11-04 17:30:55 -0500

    perfilConf e imagenes en mysql v0.1 (MinFuncional)

--- BaseDeDatos.sql
+++ BaseDeDatos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-11-2020 a las 21:39:48
+-- Tiempo de generación: 04-11-2020 a las 23:29:40
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.4
 
@@ -14,12 +14,26 @@ SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_conexionECTION=@@COLLATION_conexionECTION */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `i-com`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `infousuario`
+--
+
+CREATE TABLE `infousuario` (
+  `idInfo` int(14) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `nombre2` varchar(40) NOT NULL,
+  `apellido2` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -30,14 +44,61 @@ SET time_zone = "+00:00";
 CREATE TABLE `login` (
   `EstadoDeConexion` tinyint(1) NOT NULL,
   `usuario` varchar(20) NOT NULL,
-  `contraseña` varchar(20) NOT NULL,
+  `contrasena` varchar(20) NOT NULL,
   `Estudiante` tinyint(1) NOT NULL,
   `C_Institucional` varchar(40) DEFAULT NULL,
   `Trabajo` tinyint(1) NOT NULL,
-  `C_Empresarial` varchar(40) DEFAULT NULL
+  `C_Empresarial` varchar(40) DEFAULT NULL,
+  `CodImgPerfil` int(14) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profile_pictures`
+--
+
+CREATE TABLE `profile_pictures` (
+  `ImgCodigo` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `extension` varchar(12) NOT NULL,
+  `archivo` longblob NOT NULL,
+  `ruta` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `infousuario`
+--
+ALTER TABLE `infousuario`
+  ADD PRIMARY KEY (`idInfo`);
+
+--
+-- Indices de la tabla `profile_pictures`
+--
+ALTER TABLE `profile_pictures`
+  ADD PRIMARY KEY (`ImgCodigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `infousuario`
+--
+ALTER TABLE `infousuario`
+  MODIFY `idInfo` int(14) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `profile_pictures`
+--
+ALTER TABLE `profile_pictures`
+  MODIFY `ImgCodigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_conexionECTION=@OLD_COLLATION_conexionECTION */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- PerfilConf.php
+++ PerfilConf.php
@@ -2,9 +2,28 @@
 
 include("conexion.php");
 
-$perfil = $_POST["perfilImagen"];
-$contImage = addslashes(file_get_contents($perfil));
 
 
+if(isset($_POST["actualizarInfo"])){
+    if(!empty($perfil_FotoTMP = $_FILES['fileImg']['tmp_name'])){
+        $perfil_Foto = $_FILES['fileImg']['name'];
+        $perfil_Foto_Tipo = $_FILES['fileImg']['type'];
+        $perfil_Foto_Tamaño = $_FILES['fileImg']['size'];
+        //
+        $perfil_FotoData = addslashes(file_get_contents($perfil_FotoTMP));
+        $insertar = $conexion -> query("INSERT into profile_pictures(nombre,extension,archivo) 
+        values('$perfil_Foto','$perfil_Foto_Tipo','$perfil_FotoData')");
+        if($insertar){
+            echo("<script> alert('ok') </script>");
+        }else{
+            echo("<script> alert('error con la integracion') </script>");
+        }
+        
+    }else{
+        echo("<script> alert('no foto') </script>");
+    }
+}else{
+    echo("<script> alert('error') </script>");
+}
 
 ?>
