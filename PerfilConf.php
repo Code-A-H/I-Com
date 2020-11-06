3fdf697277bca5841529a9950c50251b8f7febe6
harvis perez <harvim.201@hotmail.com>
2020-11-06 15:14:26 -0500

    imagenes BETA v0.5

--- PerfilConf.php
+++ PerfilConf.php
@@ -9,7 +9,7 @@ if(isset($_POST["actualizarInfo"])){
         $perfil_Foto_Tipo = $_FILES['fileImg']['type'];
         $perfil_Foto_Tamaño = $_FILES['fileImg']['size'];
         $user = $_POST["user"];
-        echo "<script> alert('$user');</script>";
+        //echo "<script> alert('$user');</script>";
         //
         $perfil_FotoData = addslashes(file_get_contents($perfil_FotoTMP));
         $insertar = $conexion -> query("INSERT into profile_pictures(nombre,extension,archivo) 
@@ -18,20 +18,46 @@ if(isset($_POST["actualizarInfo"])){
             $insertar = mysqli_query($conexion,"SELECT * FROM profile_pictures WHERE nombre = '$perfil_Foto'");
             $insertar = mysqli_fetch_array($insertar);
             $ImgCod = $insertar["ImgCodigo"];
-            //echo "<script> alert('$ImgCod');</script>";
-            //$user = $_GET['usuar'];
-            //echo "<script> alert('$user');</script>";
+            
             $insertar = $conexion -> query("UPDATE login SET CodPerfil = '$ImgCod' WHERE usuario = '$user'");
-            //$insertar = mysqli_num_rows($insertar);
-            echo("<script> alert('ok'); window.location='principal.html'</script>");
+            
+            echo("<script> window.location='principal.html'</script>");
             exit;
         }else{
             echo("<script> alert('error con la integracion'); </script>");
         }
         
     }else{
-        echo("<script> alert('no foto'); window.location='PerfilConf.html'</script>");
+        $archivo = addcslashes(file_get_contents('iconoPerfil_default.png'));
+        $user = $_POST["user"];
+
+        $insertar = $conexion -> query("INSERT into profile_pictures(nombre,extension,archivo)
+        values('iconoPerfil_default','image/png','$archivo')");
+        if($insertar){
+            $insertar = mysqli_query($conexion, "SELECT * FROM profile_pictures WHERE nombre = 'iconoPerfil_default'");
+            $insertar = mysqli_fetch_array($insertar);
+            $ImgCod = $insertar["ImgCodigo"];
+            $insertar = $conexion -> query("UPDATE login SET CodPerfil = '$ImgCod' WHERE usuario = '$user'");
+            
+        }
+        
     }
+    $nombre = $_POST["Nombre"];
+    if($_POST["Nombre2"]!=null){
+        $nombre2 = $_POST["Nombre2"];
+    }
+    $apellido = $_POST["Apellido"];
+    if($_POST["Apellido2"]!=null){
+        $apellido2 = $_POST["Apellido2"];
+    }
+    if($_POST["telefono"]!=null){
+        $telefono = $_POST["telefono"];
+    }
+
+    $insertar = $conexion -> query("INSERT into infousuario(idInfo,nombre,apellido,nombre2,apellido2,Telefono)
+    values('$ImgCod','$nombre','$apellido','$nombre2','$apellido2','$telefono')");
+    echo("<script> window.location='principal.html'</script>");
+    exit;
 }
 if(isset($_POST["ver"])){
     echo("<script> alert('nice'); window.location='PerfilConf.html'</script>");
@@ -64,11 +90,18 @@ if(isset($_POST["ver"])){
                 <input type="text" style="display: none;" name="user" value="<?php echo $_GET['usuar']; ?>" >
                 <label class="let">Seleccione su imagen de perfil: </label>
                 <input type="file" id="fileImg" name="fileImg"> <br>
-                <label class="let">Nombre: </label>
+                <label class="let">Primer nombre: </label>
                 <input type="text" id="Nom" name="Nombre" required><p>*</p>
-                <br>
-                <label class="let">apellido: </label>
+                <label class="let">Segundo nombre: </label>
+                <input type="text" id="Nom2" name="Nombre2">
+                <br><br>
+                <label class="let">primer apellido: </label>
                 <input type="text" id="Ape" name="Apellido" required><p>*</p>
+                <label class="let">segundo apellido: </label>
+                <input type="text" id="Ape2" name="Apellido2">
+                <br><br>
+                <label class="let">telefono: </label>
+                <input type="text" id="tel" name="telefono">
                 <br><br>
                 <input type="submit" value="actualizar Info" name="actualizarInfo">
             </form>
